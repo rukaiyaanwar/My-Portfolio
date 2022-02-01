e73219426f513ad66005a9c0d68722de85f77b48
rukaiyaanwar <rukaiyaanwar@users.noreply.github.com>
2022-02-01 14:42:54 +0530

    first commit

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './intro.css';
+
+const Intro = () => {
+    return <div className='intro'>
+        <div className='i-left'>Left</div>
+        <div className='i-right'>Right</div>
+    </div>;
+};
+
+export default Intro;
