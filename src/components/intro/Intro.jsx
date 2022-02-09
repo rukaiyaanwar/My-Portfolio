8662703eb6b55f9cb9d3b563b387e32943c59806
rukaiyaanwar <rukaiyaanwar@users.noreply.github.com>
2022-02-09 18:14:33 +0530

    Layout Updates

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './intro.css';
+import Me from '../../img/Me.jpeg'
 
 const Intro = () => {
     return <div className='intro'>
         <div className='i-left'>
             <div className="i-left-wrapper">
                 <h2 className='i-intro'>Hello, My name is....</h2>
-                <h2 className="i-name">Rukaiya!</h2>
+                <h2 className="i-name">Rukaiya Anwar!</h2>
                 <div className="i-title">
                     <div className="i-title-wrapper">
                         <div className="i-title-item">
@@ -19,13 +20,18 @@ const Intro = () => {
                             Content Creator
                         </div>
                         <div className="i-title-item">
-                            Photographer
+                            Blogger
                         </div>
                     </div>
                 </div>
+                <div className="i-desc">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione neque mollitia at beatae est sunt unde. Repellat dicta, quos dolor, perferendis tempore soluta enim fugiat quibusdam aperiam, commodi consequatur fugit.
+                </div>
             </div>
         </div>
-        <div className='i-right'>Right</div>
+        <div className='i-right'>
+            <img src={Me} alt="" className="i-img" />
+        </div>
     </div>;
 };
 
