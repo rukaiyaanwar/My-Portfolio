42aa66e06034b8783d6353d6a5e4b55414d3eaae
rukaiyaanwar <rukaiyaanwar@users.noreply.github.com>
2022-02-09 14:29:00 +0530

    Intro move components

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,28 @@ import './intro.css';
 
 const Intro = () => {
     return <div className='intro'>
-        <div className='i-left'>Left</div>
+        <div className='i-left'>
+            <div className="i-left-wrapper">
+                <h2 className='i-intro'>Hello, My name is....</h2>
+                <h2 className="i-name">Rukaiya!</h2>
+                <div className="i-title">
+                    <div className="i-title-wrapper">
+                        <div className="i-title-item">
+                            Web Developer
+                        </div>
+                        <div className="i-title-item">
+                            UI/UX Designer
+                        </div>
+                        <div className="i-title-item">
+                            Content Creator
+                        </div>
+                        <div className="i-title-item">
+                            Photographer
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div className='i-right'>Right</div>
     </div>;
 };
