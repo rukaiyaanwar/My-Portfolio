9e68e8c39d661a3a12440d44ee86abfc2437b7c2
rukaiyaanwar <rukaiyaanwar@users.noreply.github.com>
2022-02-12 20:17:48 +0530

    Style Update

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -30,6 +30,7 @@ const Intro = () => {
             </div>
         </div>
         <div className='i-right'>
+            <div className="i-bg"></div>
             <img src={Me} alt="" className="i-img" />
         </div>
     </div>;
